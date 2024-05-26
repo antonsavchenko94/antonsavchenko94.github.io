bb8c6b82d8a5a4298152749c09750538dedd41d8
anton.savchenko <savchenko.inc@gmail.com>
2024-05-26 18:31:30 +0300

    added SearchForm component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState, memo} from 'react';
+import './SearchForm.css';
+
+export const SearchForm = memo(({ onSubmit }) => {
+    const [query, setQuery] = useState('');
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(query);
+    };
+
+    useEffect(() => {
+        console.info('Form Rendered')
+    });
+
+    return (
+        <div className="search-container">
+            <form className="search-form" onSubmit={handleSubmit}>
+                <label>
+                    Search:
+                    <input
+                        className="search-input"
+                        type="text"
+                        value={query}
+                        onChange={handleInputChange}
+                        placeholder="What would you like to eat?"
+                    />
+                </label>
+                <button type="submit" className="search-button">Search</button>
+            </form>
+        </div>
+    );
+})
+
+SearchForm.displayName = 'SearchForm';
